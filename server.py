c26b648f9b2db944fac627febf31ffbd8c3f3919
minju <ruri9898@gmail.com>
2023-11-16 16:43:26 +0900

    feature mypage

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 # 데이터 베이스 연결
 def getCon():
   return pymysql.connect(host="localhost", 
-                     user="root", password="passwd", 
+                     user="root", password="1234", 
                      db="firststep",
                      charset="utf8",
                      cursorclass=pymysql.cursors.DictCursor)
